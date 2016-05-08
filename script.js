72ad46b2f6d22acb62bf8d23375858b27b980890
Sir Karmios <nat@karmios.com>
2016-05-09 00:09:00 +0100

    Bugfix

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
     $("#convert").click(function() {
         var tl = $("#tl").val();
         var field = $("#id64");
+        var base = new BigNumber("76561197960265728");
         try {
             var id32 = parseInt(/=(\d+)&/.exec(tl)[1]);
-            var id64 = id32 + 76561197960265728;
+
+            var id64 = base.add(id32);
         } catch(err){
             field.text("Error! Make sure the trade link is correct!")
             return
         }
-        field.text(id64).focus()[0].select();
+        field.text(id64.toString()).focus()[0].select();
     });
 });
